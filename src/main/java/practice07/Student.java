4da7aea8b90086c372241b2ef7d2850006fde559
Cyril <1075887346@qq.com>
2019-07-11 23:45:16 +0800

    passed Practice07 test case5

--- src/main/java/practice07/Student.java
+++ src/main/java/practice07/Student.java
@@ -1,4 +1,22 @@
 package practice07;
 
-public class Student {
+public class Student extends Person{
+    private Klass number;
+
+    public Klass getNumber() {
+        return number;
+    }
+
+    public void setNumber(Klass number) {
+        this.number = number;
+    }
+
+    public Student(String name, int age, Klass number) {
+        super(name, age);
+        this.number = number;
+    }
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
 }
