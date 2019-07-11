35026e8342e930801f8f236d7c56920e959ee0f2
Cyril <1075887346@qq.com>
2019-07-12 06:42:03 +0800

    passed Practice09 all test cases

--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,39 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Klass number;
+
+    public Teacher(int id, String name, int age, Klass number) {
+        super(id, name, age);
+        this.number = number;
+    }
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Klass getNumber() {
+        return number;
+    }
+
+    public void setNumber(Klass number) {
+        this.number = number;
+    }
+
+    @Override
+    public String introduce() {
+        if(this.number == null){
+            return super.introduce()+" I am a Teacher. I teach No Class.";
+        }
+        return super.introduce()+" I am a Teacher. I teach "+this.number.getDisplayName()+".";
+    }
+
+    public String introduceWith(Student student){
+        if(student.getNumber().getNumber() == this.number.getNumber()){
+            return super.introduce() +  " I am a Teacher. I teach "+student.getName()+".";
+        } else {
+            return super.introduce() +  " I am a Teacher. I don't teach "+student.getName()+".";
+        }
+    }
 }
+
