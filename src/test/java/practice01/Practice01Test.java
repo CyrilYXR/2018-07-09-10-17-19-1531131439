7b97f7e7cdb5fd18d66e2317a65dc7e3da4cde65
Cyril <1075887346@qq.com>
2019-07-11 22:34:23 +0800

    pass Practice01 test case 1

--- src/test/java/practice01/Practice01Test.java
+++ src/test/java/practice01/Practice01Test.java
@@ -5,6 +5,7 @@ import org.junit.Test;
 import static org.fest.assertions.api.Assertions.assertThat;
 
 public class Practice01Test {
+    // 2min
     @Test
     public void should_person_have_name_and_age() throws Exception {
         Person person = new Person("Tom", 21);
